54eef6d523c7ba32ca3b20def71e12808574ee55
West Kraemer <kraemerwest@gmail.com>
2019-03-18 18:36:31 -0400

    if switch is on, you can press green button. If swtich is off, you can press green button.

--- trafficSegues/ViewController.swift
+++ trafficSegues/ViewController.swift
@@ -16,12 +16,17 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var segueSwitch: UISwitch!
     
+    
     @IBAction func yellowButtonTapped(_ sender: Any) {
-        
+        if !segueSwitch.isOn {
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
     }
     
     @IBAction func greenButtonTapped(_ sender: Any) {
-        
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
     }
     
     
